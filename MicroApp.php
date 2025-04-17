555952946dc24df2edcb51b2e67b153501028538
Saud bin Mohammed <samaphp@users.noreply.github.com>
2025-04-17 15:48:30 +0300

    add json response helper

--- MicroApp.php
+++ MicroApp.php
@@ -52,4 +52,11 @@ class MicroApp {
 
         return true;
     }
+
+    public static function json(array $data, int $statusCode = 200): void {
+        http_response_code($statusCode);
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    }
 }
